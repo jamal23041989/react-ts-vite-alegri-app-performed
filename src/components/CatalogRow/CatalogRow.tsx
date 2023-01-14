1fdd2c809d1457105207ad09d8c1d8b30a7483bf
jk <jk@MacBook-Air-jk.local>
2023-01-14 23:58:44 +0300

    refactoring CatalogRow

--- src/components/CatalogRow/CatalogRow.tsx
+++ src/components/CatalogRow/CatalogRow.tsx
@@ -3,35 +3,33 @@ import { useTranslation } from 'react-i18next'
 import { CustomContext } from '../../context/CustomContext'
 import './CatalogRow.scss'
 
-type Props = {}
+interface ProductsProps {
+  id: string | number
+  img: string
+  title: string
+  category: string
+  brand: string
+  price: string | number
+}
 
-export const CatalogRow = (props: Props) => {
+export const CatalogRow = () => {
   const { products } = useContext(CustomContext)
   const { i18n } = useTranslation()
 
   return (
     <div className="catalog__row">
-      {products.data.map(
-        (item: {
-          id: string | number
-          img: string
-          title: string
-          category: string
-          brand: string
-          price: string | number
-        }) => (
-          <div key={item.id} className="catalog__card">
-            <img src={item.img[0]} alt={item.title} className="catalog__card-img" />
-            <h3 className="catalog__card-title">{item.title}</h3>
-            <p className="catalog__card-category">{item.category}</p>
-            <p className="catalog__card-brand">{item.brand}</p>
-            <p className="catalog__card-price">
-              {item.price}
-              <span>{i18n.language === 'ru' ? ' руб.' : ' $'}</span>
-            </p>
-          </div>
-        )
-      )}
+      {products.data.map((item: ProductsProps) => (
+        <div key={item.id} className="catalog__card">
+          <img src={item.img[0]} alt={item.title} className="catalog__card-img" />
+          <h3 className="catalog__card-title">{item.title}</h3>
+          <p className="catalog__card-category">{item.category}</p>
+          <p className="catalog__card-brand">{item.brand}</p>
+          <p className="catalog__card-price">
+            {item.price}
+            <span>{i18n.language === 'ru' ? ' руб.' : ' $'}</span>
+          </p>
+        </div>
+      ))}
     </div>
   )
 }
